ddcd72d1e1d2dc24d78673d6ab801108a24bc306
hitesh <89287878+chawlaHitesh@users.noreply.github.com>
2025-05-27 22:13:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import checker from 'vite-plugin-checker'
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/draganddroptrello/',
     plugins: [
         react(),
         checker({
